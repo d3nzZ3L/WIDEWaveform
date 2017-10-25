85ce3c4ed153a49bb7630d574610df93be216e30
Denis Borodavchenko <denis.borodavchenko@gmail.com>
2017-10-26 02:43:30 +0300

    first commit

---
+++ WIDEWaveform/WIDEWaveform.h
@@ -0,0 +1,19 @@
+//
+//  WIDEWaveform.h
+//  WIDEWaveform
+//
+//  Created by Денис Бородавченко on 26.10.2017.
+//  Copyright © 2017 Денис Бородавченко. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WIDEWaveform.
+FOUNDATION_EXPORT double WIDEWaveformVersionNumber;
+
+//! Project version string for WIDEWaveform.
+FOUNDATION_EXPORT const unsigned char WIDEWaveformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WIDEWaveform/PublicHeader.h>
+
+
